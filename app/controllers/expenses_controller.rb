6d2b65353efef7a28b6a729e92d09f88535dad32
anderson <anderson@debian10.amr.com>
2020-11-24 17:13:34 -0500

    Implements date range filter

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,8 +1,15 @@
 class ExpensesController < ApplicationController
 	def index
-		@expenses = Expense.all.order(date: :desc)
+		@start_date = params[:start].try(:to_date) || 30.days.ago.to_date
+		@end_date = params[:end].try(:to_date) || Date.current
+
+		range = (@start_date..@end_date)
+
+		@expenses = Expense.where(date: range).order(date: :desc)
 		@column_data = Expense.types.keys.map do |type|
-			{ name: type.capitalize, data: Expense.where(type: type).group_by_week(:date).sum(:amount) }
+			{ name: type.capitalize, data: Expense.where(type: type).group_by_day(:date, range: range).sum(:amount) }
 		end
+
+		@pie_chart = Expense.where(date: range).group(:type).sum(:amount)
 	end
 end
