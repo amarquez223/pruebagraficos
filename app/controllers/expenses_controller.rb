ac83d235cbf518844566fccfca316890972d7244
anderson <anderson@debian10.amr.com>
2020-11-24 17:36:17 -0500

    Ajustes finales de formatos de etiquetas

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -7,9 +7,29 @@ class ExpensesController < ApplicationController
 
 		@expenses = Expense.where(date: range).order(date: :desc)
 		@column_data = Expense.types.keys.map do |type|
-			{ name: type.capitalize, data: Expense.where(type: type).group_by_day(:date, range: range).sum(:amount) }
+			{ name: type.capitalize, data: group_by_period(Expense.where(type: type), range).sum(:amount) }
 		end
 
 		@pie_chart = Expense.where(date: range).group(:type).sum(:amount)
+	end
+
+
+	private
+	def group_by_period(data, range)
+		diff = @end_date - @start_date
+		if diff < 31
+			data.group_by_day(:date, range: range, format: "%b %d")
+		elsif diff < 91
+			data.group_by_week(:date, range: range, format: Proc.new { |date| format_week_label(date) })
+		else
+			data.group_by_month(:date, range: range, format: "%b %Y")
+		end
+	end
+
+	def format_week_label(date)
+		start_date = date.to_date
+		end_date = start_date + 6
+		label = "#{start_date.strftime('%b %d')} - "
+		label += start_date.month == end_date.month ? end_date.strftime('%d') : end_date.strftime('%b %d')
 	end
 end
